4d1de24ae8601638a88e2697ff8895fc90f9c129
dawon222 <da5720@naver.com>
2024-08-02 16:46:55 +0900

    커밋 메세지

--- src/router/user.ts
+++ src/router/user.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
 import { UserService } from "../service/user.ts";
 import { ResponseType } from "../types/response.type.ts";
-import { UserSchema } from "../schema/user.js";
-import { UserType } from "../types/user.type.js";
+import { UserSchema } from "../schema/user.ts";
+import { UserType } from "../types/user.type.ts";
 
 const UserRouter = Router();
 const userservice: UserService = new UserService();
@@ -18,3 +18,33 @@ UserRouter.post("/", async (req, res) => {
 
   res.json(result);
 });
+
+UserRouter.get("/:id",async(req,res)=>{
+  const id:String=req.params.id;
+  const user=await userservice.get(id);
+
+  if(!user){
+    return res.status(501).json({
+      message:"사용자를 찾을 수 없습니다.",
+    });
+  }
+
+  const result: ResponseType<UserType>={
+    message:"성공적으로 조회되었습니다.",
+    data:user,
+  };
+  return res.json(result);
+});
+
+UserRouter.delete("/:id",async(req,res)=>{
+  const id:String=req.params.id;
+  const user=await userservice.delete(id);
+
+  const result: ResponseType<UserType>={
+    message:"성공적으로 삭제되었습니다.",
+    data:user,
+  };
+  res.json(result);
+});
+
+export default UserRouter;
--- src/service/user.ts
+++ src/service/user.ts
@@ -1,4 +1,5 @@
-import { UserModel } from "../schema/user";
+import { UpdateWriteOpResult, DeleteResult } from "mongoose";
+import { UserModel } from "../schema/user.ts";
 import { UserType } from "../types/user.type";
 
 export class UserService {
@@ -6,8 +7,33 @@ export class UserService {
     try {
       const user = await UserModel.create(data);
       return user as UserType;
-    } catch (err) {
+    } 
+    catch (err) {
+      throw err;
+    }
+  }
+  async get(userId:String):Promise<UserType>{
+    const user=await UserModel.findOne({id: userId});
+    return user as UserType;
+  }
+  async patch(userId:String,data:String):Promise<UpdateWriteOpResult>{
+    try{
+      const user=await UserModel.updateOne({id:userId},data);
+      return user;
+    }
+    catch(err){
+      throw err;
+    }
+  }
+  async delete(userId:String):Promise<DeleteResult>{
+    try{
+      const user=await UserModel.deleteOne({id:userId});
+      return user;
+    }
+    catch(err){
       throw err;
     }
   }
 }
+
+
